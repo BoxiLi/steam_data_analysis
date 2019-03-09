81595125850569fbf7f84e59a37c852f1b05c30f
Kane2009 <48367323+Kane2009@users.noreply.github.com>
2019-03-09 17:37:23 +0800

    Update steam_id.py

--- steam_id.py
+++ steam_id.py
@@ -1,16 +1,25 @@
-def NextLevel(id_parent):
+def NextLevel(idp, id_parent):
     steamid_son = []
     for i in range(len(id_parent)):
-        n=0
         parent = steamapi.user.SteamUser(id_parent[i])
         try:
             fr = parent.friends
+            n=len(idp)
             for j in range(len(fr)):
-                steamid_son.append(fr[j].id)
+                ind = 0
+                for k in range(n):
+                    if fr[j].id == idp[k]:
+                        ind = ind+1
+                if ind == 0:
+                    steamid_son.append(fr[j].id)
+                    idp.append(fr[j].id)
         except:
             continue
-    return steamid_son
+    return [steamid_son, idp]
 def main():
     steam_id = [[me.id]]
+    idp = [me.id]
     for i_o in range(3):
-        steam_id.append(NextLevel(steam_id[i_o]))
+        ex = NextLevel(idp, steam_id[i_o])
+        idp = ex[1]
+        steam_id.append(ex[0])
