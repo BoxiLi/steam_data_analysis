862cbaa863bff0d36906104c6f452d261d6a1fbb
Boxi Li <etamin1201@gmail.com>
2019-03-10 20:41:59 +0100

    debug: duplication
    steam_id_set must be loaded at the begining

--- steam_id.py
+++ steam_id.py
@@ -23,49 +23,42 @@ def steam_search(steam_id_set, search_id_list, id_file_name, num_result):
         except:
             continue
     
-    # If result from previous search exist, read it, otherwise create a new one
-    try:
-        id_data = pd.read_csv(id_file_name+".csv")
-    except:
-        id_data = pd.DataFrame(columns=['id'])
-    
     # write search_id_list to a csv file
     id_search_data = pd.DataFrame(list(search_id_list), columns=['id'])
     id_search_data.to_csv(id_file_name + "_search.csv", index=False)
 
     # write steam_id_set to a csv file
     # can this be done without a new copy?
-    new_id_data = pd.DataFrame(list(steam_id_set), columns=['id'])
-    id_data = id_data.append(new_id_data)
+    id_data = pd.DataFrame(list(steam_id_set), columns=['id'])
     id_data.to_csv(id_file_name+".csv", index=False)
             
             
 
 
+# This seciton will not run if the py script is imported by another
+steamapi.core.APIConnection(api_key="6B61866E0CAEBE0BE9804CAAB54502E9", validate_key=True)
+root = steamapi.user.SteamUser(userurl="kane2019")
 
-if __name__ == "__main__":
-    # This seciton will not run if the py script is imported by another
-    steamapi.core.APIConnection(api_key="6B61866E0CAEBE0BE9804CAAB54502E9", validate_key=True)
-    root = steamapi.user.SteamUser(userurl="kane2019")
+# parameters
+id_file_name = "test" # The file name where the data will be stored
+num_result = 50 # The approxmated number of result of result in this search
 
-    # parameters
-    id_file_name = "test" # The file name where the data will be stored
-    num_result = 50 # The approxmated number of result of result in this search
+# The following two variable can later also be imported from a csv file:
+#
+# steam_id_set: steam_id_set saves the result id, this is a set object, which is not ordered 
+#               but elements can be efficiently searched. No duplication can exist in a set.
+# search_id_list: search_id_list saves the id that whose friends remain to be searched. It is a list object.
+#               All elements in this list already exists in steam_id_set
 
-    # The following two variable can later also be imported from a csv file:
-    #
-    # steam_id_set: steam_id_set saves the result id, this is a set object, which is not ordered 
-    #               but elements can be efficiently searched. No duplication can exist in a set.
-    # search_id_list: search_id_list saves the id that whose friends remain to be searched. It is a list object.
-    #               All elements in this list already exists in steam_id_set
+# If the data exist, read the existing search_id_list data, otherwise create one with the root id
+try:      
+    id_search_data = pd.read_csv(id_file_name+"_search.csv")
+    search_id_list = id_search_data["id"].tolist()
+    id_data = pd.read_csv(id_file_name+".csv")
+    steam_id_set = set(id_data["id"].tolist())
 
-    # If the data exist, read the existing search_id_list data, otherwise create one with the root id
-    try:      
-        id_search_data = pd.read_csv(id_file_name+"_search.csv")
-        search_id_list = id_search_data["id"].tolist()
-        steam_id_set = set()
-    except:
-        steam_id_set = {root.id}
-        search_id_list = [root.id]
-    steam_search(steam_id_set, search_id_list, id_file_name, num_result)
+except:
+    steam_id_set = {root.id}
+    search_id_list = [root.id]
+steam_search(steam_id_set, search_id_list, id_file_name, num_result)
     
