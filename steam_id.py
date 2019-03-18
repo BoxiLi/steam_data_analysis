48b819590a2be3892bc4ef9c05a469ce320900ac
Boxi Li <etamin1201@gmail.com>
2019-03-18 19:44:12 +0100

    add stem_info function

--- steam_id.py
+++ steam_id.py
@@ -34,6 +34,47 @@ def steam_search(steam_id_set, search_id_list, id_file_name, num_result):
         writer.writerow(steam_id_set)
             
 
+def steam_info(steam_id_set, steam_info_data, id_file_name):
+    """
+    This function take a set of steam id and call steamapi to get the detailed information
+    of them. The result is saved in a dicationary steam_info_data and in a json file.
+    """
+    count = 0
+    for id in steam_id_set:
+        # If this id has been searched already, ignore it (incase we want to increase datasize)
+        if str(id) in steam_info_data.keys():
+            continue
+
+        # Call the required information
+        user = steamapi.user.SteamUser(id)
+        try:
+            friends = [friend.id for friend in user.friends]
+            games = [game.id for game in user.games] # we save only the integer id
+            date = user.time_created
+            time_created = (date.year, date.month, date.day)
+            level = user.level
+        except (steamapi.errors.APIUnauthorized, steamapi.errors.AccessException):
+            # If the user information is not accessable, return empty data. We would like it's 
+            # key to remain in steam_info_data to prevent searching it again in the future.
+            friends = [] 
+            games = []
+            time_created = None
+            level = None
+        
+
+        steam_info_data[id] = {"games":games, "friends":friends, "time_created":time_created,
+            "level":level}
+
+        # Saving during the process to prevent losing data
+        count += 1
+        if count%100 == 0:
+            print(count, "IDs have been searched and saved")
+            with open(id_file_name + '.json', 'w') as f:
+                json.dump(steam_info_data, f)
+
+    # Saving all the data
+    with open(id_file_name + '.json', 'w') as f:
+        json.dump(steam_info_data, f)
 
 
 
@@ -43,13 +84,21 @@ root = steamapi.user.SteamUser(userurl="kane2019")
 
 # parameters
 id_file_name = "test" # The file name where the data will be stored
-num_result = 500 # The approxmated number of result of result in this search
+num_result = 5 # The approxmated number of result of result in this search
 
 # Important variables:
 # steam_id_set: steam_id_set saves the result id, this is a set object, which is not ordered 
 #               but elements can be efficiently searched. No duplication can exist in a set.
 # search_id_list: search_id_list saves the id that whose friends remain to be searched. It is a list object.
 #               All elements in this list already exists in steam_id_set
+# steam_info_data: steam_info_data is a dictionary which uses the str(id) as key and contains information of 
+#               the user. Currently the informaiton includes:
+#               "games": a list of game id (integer)
+#               "friends": a list of friends id (integer)
+#               "level"：an integer
+#               "time_created": a tuple containing three integer (year, month, day)
+#               CAREFUL: JSON only allow string to be the keys, thus the keys are always str(id) instead of an integer!
+#
 
 # If the data exist, read the existing search_id_list data, otherwise create one with the root id
 try: 
@@ -61,6 +110,7 @@ try:
                 search_id_list.append(int(row[0]))
             except IndexError:
                 pass
+
     steam_id_set = set()
     with open(id_file_name + ".csv", 'r') as f:
         reader = csv.reader(f, delimiter = '\n')
@@ -71,8 +121,14 @@ try:
             except IndexError:
                 pass
 
+    json_data = open(id_file_name + ".json").read()
+    steam_info_data = json.loads(json_data)
+
 except FileNotFoundError:
     steam_id_set = {root.id}
     search_id_list = [root.id]
+    steam_info_data = {}
+
 steam_search(steam_id_set, search_id_list, id_file_name, num_result)
-    
+
+steam_info(steam_id_set, steam_info_data, id_file_name)
--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-a = {"boxi": 1}
-data = {"a":a}
-
-with open('data.json', 'w') as fp:
-    json.dump(data, fp)
-
-open("adfasd")
