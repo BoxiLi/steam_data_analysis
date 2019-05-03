8e8f633c0d15810187ec16a56e880b5a5fd1e5c4
Kane2009 <Kane2009@users.noreply.github.com>
2019-05-04 05:15:23 +0800

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 from data_preparation import *
 
 
+
 def pow_normalize(mat):
     return mat/60**(1./3.)
 
 
-def SVD(mat, feature = 20, step = 1000, Rate = 0.0000001, Type = 0, ItemFeature = [0]):
-
+def SVD(mat, feature = 20, step = 1000, Rate = 0.00001, Type = 0, ItemFeature = [0]):
+    
     ARmse = np.inf
     Rmse = 0.0
     lr = Rate
@@ -16,6 +17,7 @@ def SVD(mat, feature = 20, step = 1000, Rate = 0.0000001, Type = 0, ItemFeature 
     L = [[],[]]
     error = 0.0
     x=0.0
+    np.random.seed(0)
     UserFeature = np.matrix(np.random.rand(mat.shape[0], feature))
     if Type == 0:
         ItemFeature = np.matrix(np.random.rand(mat.shape[1], feature))
@@ -50,7 +52,9 @@ def SVD(mat, feature = 20, step = 1000, Rate = 0.0000001, Type = 0, ItemFeature 
             break
     LearningProcess = L
     Result = np.dot(UserFeature, ItemFeature.T)
-    return UserFeature, ItemFeature, LearningProcess, Result
+    return UserFeature, ItemFeature, LearningProcess, Result,  ARmse
+
+
 
 
 def ItemBase(OwnItem, ItemFeature, NNN):
@@ -110,7 +114,7 @@ result = SVD(mat)
 """
 
 
-
+# This part for Evaluation
 
 def user_filter(file_name, Lower_limit = 50, User = 1000):
     json_data = open(file_name + ".json", 'r').read()
@@ -122,7 +126,7 @@ def user_filter(file_name, Lower_limit = 50, User = 1000):
         game_nonz = []
         for gameid, time in user_game:
             if not time == 0:
-                game_nonz.append(time)
+                game_nonz.append(time)    
         mean = np.mean(game_nonz)
         user_game_norm = []
         if len(user_game) >= Lower_limit:
@@ -167,6 +171,76 @@ def evaluation(itemfeature, library, Userdata, n = 20, feature1 = 20, rate = 0.1
         Rmse = Rmse + pow(error/n, 2)
     rmse = np.sqrt(Rmse)
     return rmse
+# end of evaluation part
+
+
+def SVD_2(mat, game_list, user, feature = 20, step = 1000, Rate = 0.00001, Type = 0, ItemFeature = [0]):
+
+    # Calculate ARmse step by step
+
+    ARmse = np.inf
+    Rmse = 0.0
+    lr = Rate
+    Lambda = 0.01
+    L = [[],[]]
+    error = 0.0
+    x=0.0
+    rmse_for_eva = []
+    UserFeature = np.matrix(np.random.rand(mat.shape[0], feature))
+    if Type == 0:
+        ItemFeature = np.matrix(np.random.rand(mat.shape[1], feature))
+    row_indices, col_indices = mat.nonzero() # Returns a tuple of arrays (row,col) containing the indices of the non-zero elements of the matrix.
+    useful_entry_number = len(row_indices)
+    for i in range(step):
+        rmse = 0.0
+        for entry_iter in range(useful_entry_number):
+            p = row_indices[entry_iter]
+            q = col_indices[entry_iter]
+            Pred = np.dot(UserFeature[p,:], ItemFeature[q,:].T)
+            error = mat[p,q] - Pred
+            #print("error = ",error)
+            rmse = rmse + pow(error/useful_entry_number, 2)
+            #type0 for the whole data set, type1 for new userdata.  
+            if Type == 0:
+                UserFeature[p] = UserFeature[p] + lr * (error * ItemFeature[q] - Lambda * UserFeature[p])
+                ItemFeature[q] = ItemFeature[q] + lr * (error * UserFeature[p] - Lambda * ItemFeature[q])
+            elif (Type == 1):
+                UserFeature[p] = UserFeature[p] + lr * (error * ItemFeature[q] - Lambda * UserFeature[p])
+       #type0 for the whole data set, type1 for new userdata.                     
+        Rmse = np.sqrt(rmse)
+        print("Rmse = ", Rmse, "ARmse = ", ARmse)
+        L[0].append(x+i*lr)
+        L[1].append(Rmse)
+        rmse_for_eva.append(evaluation(ItemFeature, game_list, user))
+
+        if Rmse < ARmse:
+            ARmse = Rmse
+       #     lr = lr*0.99
+       #     lr = lr/(1.0 + 0.0001*i)
+       # if abs(Rmse)<0.1:
+        else:
+            break
+    LearningProcess = L
+    Result = np.dot(UserFeature, ItemFeature.T)
+    return UserFeature, ItemFeature, LearningProcess, Result,  ARmse, rmse_for_eva
+
+def main(type = 0):
+    file_name = "user_game30k"
+    file_name2 = "user_game300k"  # providing data for evaluating
+    generator = user_game_matrix(file_name)
+# default of "played_required" is True
+    generator.played_required = None
+    generator.thres_game = None
+    generator.thres_user = None
+    generator.normalize_func = tanh_normalize
+    mat, user_list, game_list = generator.construct()  
+    user = user_filter(file_name2, User = 20)
+    if type == 0:
+        rmse_for_eva = SVD_2(mat, game_list, user, Rate = 0.001) # records of errors step by step
+    elif type == 1:
+        rmse_for_feature = SVD(mat, feature = 19, Rate = 0.001) # records of errors for different feature number
+        
+
 """    
 import json
 file_name= "user_game30k"
