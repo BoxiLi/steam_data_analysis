b0a791d62687caa192b6d8351676cd1fe50d17f2
Boxi Li <etamin1201@gmail.com>
2019-05-18 13:25:31 +0200

    Update eval_boxi.py

--- eval_boxi.py
+++ eval_boxi.py
@@ -16,15 +16,18 @@ def lsq(A, b):
 def eval_pred_error(recommender, hole_indices, hole_values):
     pred_error2 = 0.0
     num_samples = len(hole_indices)
+    pred_list = np.empty(num_samples)
     for i in range(num_samples):
         p,q = hole_indices[i]
-        pred = np.dot(recommender.UserFeature[p].T , recommender.ItemFeature[q])
-        pred_error2 += (hole_values[i] -pred)**2
+        pred_list[i] = recommender.mu + recommender.UserUncertain[p] + recommender.ItemUncertain[q] + np.dot(recommender.UserFeature[p].T, recommender.ItemFeature[q])
+    
+    pred_error2 = np.sum(np.square(hole_values - pred_list))
     pred_error = np.sqrt(pred_error2/(num_samples-1))
-    return pred_error
+    corr = np.corrcoef(pred_list, hole_values)[0,1]
+    return pred_error, corr
 
 
-def dig_hole_evaluation(feature_num = 20, step_num = 10, rate = 0.001, num_samples = 100, regu = 0.01, file_name = "user_game30k", seed = None):
+def dig_hole_evaluation(feature_num = 20, step_num = 10, rate = 0.001, num_samples = 300, regu = 0.01, file_name = "user_game30k", seed = None):
     if seed is not None:
         np.random.seed(seed)
     generator = user_game_matrix(file_name)
@@ -40,7 +43,7 @@ def dig_hole_evaluation(feature_num = 20, step_num = 10, rate = 0.001, num_sampl
 
     pointers = np.random.choice(range(num_total_entries), num_samples, replace=False) # replace = False, no repetition
     hole_indices = list(zip(row_indices[pointers], col_indices[pointers])) # create coordiante pairs [(r1,c1),(r2,c2)...]
-    hole_values = [mat[index] for index in hole_indices] # record existing mat entries
+    hole_values = np.array([mat[index] for index in hole_indices]) # record existing mat entries
     for index in hole_indices:
         mat[index] = 0. # set to 0, delete it from the sparse mat so that it will be fitted)
 
@@ -48,44 +51,96 @@ def dig_hole_evaluation(feature_num = 20, step_num = 10, rate = 0.001, num_sampl
     recommender = SVD_recommender(mat, feature_num, regu)
     learn_procress = [np.inf]*5 # inf just for compare, will be deleted at the end
     eval_process = []
+    corr_list = []
     row_indices, col_indices = recommender.mat.nonzero()
     for i in range(step_num):
         rmse = recommender.svd_step(rate, row_indices, col_indices)
-        pred_error = eval_pred_error(recommender, hole_indices, hole_values)
+        pred_error, corr = eval_pred_error(recommender, hole_indices, hole_values)
         # print("rmse =", rmse, "Eval =", pred_error)
         learn_procress.append(rmse)
         eval_process.append(pred_error)
-        if rmse > learn_procress[-5]:
-            break
-    return recommender.UserFeature, recommender.ItemFeature, learn_procress[5:], eval_process
+        corr_list.append(corr)
+        if i%50==0:
+            print("{} steps finished".format(i))
+        # if rmse > learn_procress[-5]:
+        #     break
+    return learn_procress[5:], eval_process, corr_list
 
 
-def test_process(regu):
-    return dig_hole_evaluation(feature_num = 20, step_num = 400, rate = 0.001, num_samples = 100, regu = regu, file_name = "user_game30k", seed = 0)
+# def test_process(regu):
+#     return dig_hole_evaluation(feature_num = 20, step_num = 3000, rate = 0.001, num_samples = 100, regu = regu, file_name = "user_game30k", seed = 6)
 
+
+# if __name__ =="__main__":
+#     regu_list = np.linspace(0.00, 0.1, 6)
+#     process_num = min(len(regu_list), 11)
+#     with Pool(process_num) as p:
+#         result = p.map(test_process, regu_list)
+
+#     fig1 = plt.figure(dpi=200)
+#     ax1 = fig1.subplots()
+#     fig2 = plt.figure(dpi=200)
+#     ax2 = fig2.subplots()
+#     fig3 = plt.figure(dpi=200)
+#     ax3 = fig3.subplots()
+#     for i in range(len(regu_list)):
+#         learn_procress = result[i][0]
+#         eval_process = result[i][1]
+#         corr = result[i][2]
+#         ax1.plot(learn_procress, label = "regu {}".format(regu_list[i]))
+#         ax2.plot(eval_process, label = "regu {}".format(regu_list[i]))
+#         ax3.plot(corr, label = "regu {}".format(regu_list[i]))
+#     ax1.set_xlabel("step")
+#     ax2.set_xlabel("step")
+#     ax3.set_xlabel("step")
+#     ax1.set_ylabel("Loss")
+#     ax2.set_ylabel("prediction error")
+#     ax3.set_ylabel("correlation")
+#     ax1.legend()
+#     ax2.legend()
+#     ax3.legend()
+#     ax1.set_title("Loss function for different regularization")
+#     ax2.set_title("Evaluation for different regularization")
+#     ax3.set_title("Correlation for different regularization")
+#     plt.show()
+
+
+def test_process(feature_num):
+    return dig_hole_evaluation(feature_num, step_num = 3000, rate = 0.001, num_samples = 1000, regu = 0.01, file_name = "user_game300k", seed=300)
 
 if __name__ =="__main__":
-    fig1 = plt.figure()
+    # feature_num_list = np.linspace(2, 10, 5).astype(int)
+    feature_num_list = [1,3,5,7,9,11]
+    process_num = min(len(feature_num_list), 11)
+
+
+    with Pool(process_num) as p:
+        result = p.map(test_process, feature_num_list)
+
+
+    fig1 = plt.figure(dpi=200)
     ax1 = fig1.subplots()
-    fig2 = plt.figure()
+    fig2 = plt.figure(dpi=200)
     ax2 = fig2.subplots()
-
-    regu_list = np.linspace(0.01,0.05, 5)
-    with Pool(len(regu_list)) as p:
-        result = p.map(test_process, regu_list)
-
-    for i in range(len(regu_list)):
-        learn_procress = result[i][2]
-        eval_process = result[i][3]
-        ax1.plot(learn_procress, label = "regu {}".format(regu_list[i]))
-        ax2.plot(eval_process, label = "regu {}".format(regu_list[i]))
-
+    fig3 = plt.figure(dpi=200)
+    ax3 = fig3.subplots()
+    for i in range(len(feature_num_list)):
+        learn_procress = result[i][0]
+        eval_process = result[i][1]
+        corr = result[i][2]
+        ax1.plot(learn_procress, label = "F {}".format(feature_num_list[i]))
+        ax2.plot(eval_process, label = "F {}".format(feature_num_list[i]))
+        ax3.plot(corr, label = "F {}".format(feature_num_list[i]))
     ax1.set_xlabel("step")
     ax2.set_xlabel("step")
-    ax1.set_ylabel("rmse")
+    ax3.set_xlabel("step")
+    ax1.set_ylabel("Loss")
     ax2.set_ylabel("prediction error")
+    ax3.set_ylabel("correlation") 
     ax1.legend()
     ax2.legend()
-    ax1.set_title("rmse as function of the step number for different regu")
-    ax2.set_title("Evaluation as function of the step number for different regu")
+    ax3.legend()
+    ax1.set_title("Loss function for different feature number")
+    ax2.set_title("Evaluation for different feature number")
+    ax3.set_title("Correlation for different feature number")
     plt.show()
